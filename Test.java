d17905bebdc054fa1c53e119145f9d679ab22015
firdauzy <nuzulafirdhauzy@gmail.com>
2025-09-24 11:03:50 +0700

    halobro

--- Test.java
+++ Test.java
@@ -3,5 +3,6 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("hi nama saya robby dio di panggil ambon");
+        System.out.println("hi nama saya prabowo di panggil wowok");
     }
 }
